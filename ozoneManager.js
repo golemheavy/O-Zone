e3052b3f527891932f0cc3d97129e5772576181e
Steve Long <steve@gridtop.com>
2019-05-16 22:32:20 -0500

    almost working

--- ozoneManager.js
+++ ozoneManager.js
@@ -37,7 +37,6 @@ function start() {
 	{
 		type: 'list',
 		name: 'selectOption',
-		message: 'Option',
 		choices: ["View Products","View Low Inventory","Add to Inventory","Add New Product"]
 	}
 	];
@@ -50,54 +49,158 @@ function start() {
 			case "Add New Product": addProduct(); break;
 			default: break;
 		}
-		connection.end();
 	});
 }
 
-function viewProducts() {
-	const qry = "SELECT * FROM products;";
-	executeQuery(qry);	
-	// * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+function promptForContinue() {
+	inquirer.prompt([
+	{
+		type	: 'confirm',
+		name	: 'continue',
+		message	: 'Would you to continue?',
+	}
+	]).then(answers => {
+		if (answers.continue) start();
+		else connection.end();
+	});
 }
 
-function viewLowInventory() {// * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
-	const qry = "SELECT * FROM products WHERE  QTY_in_stock < 5;";
-	executeQuery(qry);
+function viewProducts() { // list every available item: the item IDs, names, prices, and quantities.
+	const qry = "SELECT * FROM products;";
+	executeQuery(qry);	
+}
+
+function viewLowInventory() {// list all items with an inventory count lower than five.
+	const qry = "SELECT * FROM products WHERE QTY_in_stock < ?";
+	const arr = ["5"];
+	executeQuery(qry,arr);
 }
 
 function addInventory() {
-	const qry = "";
-	arr = [];
-	executeQuery(qry, arr);	
-	// * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+	//prompt for which item and how many to increase it by
+		
+connection.query("SELECT CONCAT('item_id ', item_id, ' ', product_name, ' qty: ', QTY_in_stock) AS product FROM products;",[],function(err, res){
+	if (err) throw err;
+	let choiceArr = [];
+	for (x in res) {
+		choiceArr.push(res[x].product);
+	}
+	var questions = [
+	{
+		type: 'list',
+		name: 'selectItem',
+		choices: choiceArr  // load the choices dynamically
+	},
+	{
+    type: 'input',
+    name: 'addUnits',
+	message: 'What would you like to change the quantity to?',
+    //message: 'How many units would you like to add?',  FIX THIS -- should let you add to, not overwrite
+    validate: function(value) {
+      var valid = !isNaN(parseFloat(value));
+      return valid || 'Please enter a number';
+    },
+    filter: Number
+	}
+	];
+	inquirer.prompt(questions).then(answers => {
+		console.log(answers);
+		const qry = "update products set QTY_IN_STOCK = ? where ITEM_ID = ?";
+		const arr = [answers.addUnits.toString(),answers.selectItem.split(" ")[1]];
+		console.log(qry);
+		console.log(arr);
+		connection.query(qry,arr,function(err, res){
+			if (err) throw err;
+			//console.log(res);
+			console.log('\nExecuted query:\t"' + qry + '" with values:\t' + function(){if (typeof arr === "undefined" || arr.length === 0) return "none"; else return arr.toString();}());
+			console.log("result:");
+			console.log(res.message + ")");
+			promptForContinue();
+		});
+	});
+	});
 }
 
-function addProduct() {
-	const qry = "";
-	arr = [];
-	executeQuery(qry, arr);	
-	// * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+function addProduct() { // allow user to add a completely new product to the store.
+	
+	connection.query("DESCRIBE PRODUCTS",[],function(err, res){
+		if (err) throw err;
+		if (typeof res !== "undefined" && res.length > 0) {
+			let intFieldNameArr = [];
+			let stringFieldNameArr = [];
+			let x;
+			for (x in res) {
+				if (res[x].Key === "" && res[x].Type.startsWith("int")) intFieldNameArr.push(res[x].Field);
+				else if (res[x].Key === "" && res[x].Type.startsWith("varchar")) stringFieldNameArr.push(res[x].Field);
+			}
+			
+			//console.log(intFieldNameArr);
+			//console.log(stringFieldNameArr);
+			
+			let y, z = 0;
+			let questions = [];
+			for (y in stringFieldNameArr) {
+				questions.push({type: "input", name: stringFieldNameArr[y], message: "Please enter value for " + stringFieldNameArr[y]});
+			}
+			for (z in intFieldNameArr) {
+				questions.push({
+					type: "input",
+					name: intFieldNameArr[z],
+					message: "Please enter value for " + intFieldNameArr[z],
+					validate: function(value) {
+						var valid = !isNaN(parseFloat(value));
+						return valid || 'Please enter a number';
+					},
+					filter: Number});
+			}
+			
+			inquirer.prompt(questions).then(answers => {
+				console.log(answers);
+				const qry = "INSERT INTO PRODUCTS (?) VALUES (?);";
+				const columnNames = stringFieldNameArr.toString() + "," + intFieldNameArr.toString();
+				const valuesArr = [];
+				let a = 0;
+				let columnNamesArr = columnNames.split(",");
+				for (a in columnNamesArr) {
+					valuesArr.push(answers[columnNamesArr[a]]);
+				}
+				const arr = [columnNames, valuesArr.join(",")];
+				console.log("qry:");
+				console.log(qry);
+				console.log("arr:");
+				console.log(arr);
+				//executeQuery(qry, arr);	
+			});
+		}
+		
+	});
 }
 
 function executeQuery(qry, arr) {
 	//this function executes the query 
-	if (!arr) arr = [];
+	//if (typeof arr === "undefined") arr = [];
 	connection.query(qry,arr,function(err, res){
 		if (err) throw err;
+		if (typeof res !== "undefined" && res.length > 0) { // this checks to ensure that res is not an empty array
 		
-		let columns = Object.keys(res[0]);
-		let table = new Table({ head: columns });
-		let x;
-		for (x in res) {
-			let rowArr = [];
-			for (y in columns) {
-				rowArr.push(res[x][columns[y]]);
+		let printRes = 
+		function(){
+			let columns = Object.keys(res[0]);
+			let table = new Table({ head: columns });
+			let x;
+			for (x in res) {
+				let rowArr = [];
+				for (y in columns) {
+					rowArr.push(res[x][columns[y]]);
+					}
+				table.push(rowArr);
 			}
-			table.push(rowArr);
-		}
-				
-		console.log(table.toString());
+			console.log(table.toString());
+		}();
 		
-		console.log('\nExecuted query:\t"' + qry + '" with values:\t' + function(){if (arr.length === 0) return "none"; else return arr.toString();}());
+		console.log('\nExecuted query:\t"' + qry + '" with values:\t' + function(){if (typeof arr === "undefined" || arr.length === 0) return "none"; else return arr.toString();}());
+		}
+		else console.log ("\nno results.\n");
+		promptForContinue();
 	});
 }
